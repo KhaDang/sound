70ce9f7997678ac9b10d35d0f62aab0c1cdd0958
KhaDang <dminhkha@gmail.com>
2018-03-19 13:21:28 +0000

    second commit

---
+++ main.c
@@ -0,0 +1,17 @@
+// this file contains the main function of our project
+
+#include "sound.h" 
+#include <stdlib.h>
+
+int main(void){
+	while(1){
+		// Run system command "arecord" to record 1 sec of wave
+		system("arecord -r8000 -c1 -f S16_LE -d1 test.wav");
+		
+		// Display WAV header
+		dispWAVHeader("test.wav");
+		// Display WAV strength as decibel values
+		break;			// loop runs just once
+	}
+	return 0;
+}
