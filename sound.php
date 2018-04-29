1485b4db86650d0fc7c244bed83cf4280d464594
KhaDang <37361246+KhaDang@users.noreply.github.com>
2018-04-29 23:15:17 +0300

    Add files via upload

---
+++ sound.php
@@ -0,0 +1,20 @@
+<?php
+$log_file = "sound_log.txt";
+$data = $_POST["data"];
+$today = date("Y-m-d H:i:s");
+$str = $today.", ".$data."\n";
+$myData = json_encode($str);
+echo $myData;
+
+if(file_exists($log_file)){
+		$fp = fopen($log_file, a); // the file is opened as append mode
+		fwrite($fp, $str);
+		fclose($fp);
+}else{ // if the file doesn't exist, create a new file
+	$fp = fopen($log_file, w); // the file is opened as write mode
+	fwrite($fp, $str);
+	fclose($fp);
+}
+
+echo "Data is stored.\n";
+?>
